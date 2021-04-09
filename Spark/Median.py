34b4c05914158e96584f1fc82f13de39f9a2d938
Faider Florez Valencia <faiderfl@outlook.com>
2021-04-09 12:01:33 -0500

    Sorting

--- Spark/Median.py
+++ Spark/Median.py
@@ -4,3 +4,6 @@ from pyspark.sql import Window
 windowSpec = Window.partitionBy(df['Role'])
 df2 = df.withColumn('mean_salary', func.mean(df['Salary']).over(windowSpec))
 df2.show()
+
+SELECT approx_percentile(10.0, array(0.5, 0.4, 0.1), 100);
+SELECT approx_percentile(val, 0.5, 100);
